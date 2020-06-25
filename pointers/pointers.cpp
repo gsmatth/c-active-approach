b7ca61efe4437b8632c16d133d9ea834f3b7a631
Doug Popadince <gsmatth@icloud.com>
2020-06-25 15:25:50 -0700

    added passing functions pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib> //for qsort function
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::numeric_limits;
+using std::streamsize;
+
 
 
 void GetFraction(int * numerator, int * denominator);
@@ -13,9 +17,19 @@ void PointerMath();
 void DisplayArrayContent(int rows, int columns);
 void DeleteCStringSection(char * cStringArray, int start_index, int num_char_to_delete);
 int CharAt(char * str, char ch);
+void StringTokSample();
+void StringInString();
+void CharInString();
+void GetString(char *& strA);
+int compare_ints(const void * arg1, const void * arg2);
+int compare_strs(const void * arg1, const void * arg2);
+
+
+
+
+const int BUFFER_LENGTH = 256;
 
 int main(){
-
 float average = 20.0F;
 float * flt_ptr = &average;
 //declaring and initializing a null pointer
@@ -67,26 +81,122 @@ cout << "address stored in int_ptr: " << int_ptr << endl;//0x7ffeefbffa98
 
 
 int numerator = 0, denominator = 0;
-GetFraction(&numerator, &denominator);
-PointerMath();
-DisplayArrayContent(4,6);
+// GetFraction(&numerator, &denominator);
+// PointerMath();
+// DisplayArrayContent(4,6);
 
-//-----------------------Pointer Math------------------------------------//
+//-----------------------Pointer Math Begin------------------------------------//
 
-char test_string[50] = "this is the initialized cString";
-char * test_ptr = test_string; 
-cout << "This is test_string before DeleteCStringSectionfunction is called: " << test_string << endl;
+// char test_string[50] = "this is the initialized cString";
+// char * test_ptr = test_string; 
+// cout << "This is test_string before DeleteCStringSectionfunction is called: " << test_string << endl;
 
-DeleteCStringSection(test_ptr, 0, 6);
+// DeleteCStringSection(test_ptr, 0, 6);
 
-cout << "This is test_string after copy: " << test_string << endl;
+// cout << "This is test_string after copy: " << test_string << endl;
 
-int char_at = CharAt(test_ptr, 'c');
-cout << "The charAt output is: " << char_at << endl;
+// int char_at = CharAt(test_ptr, 'c');
+// cout << "The charAt output is: " << char_at << endl;
+
+
+//-----------------------Pointer Math End------------------------------------//
+
+
+
+//------------------------cString Functions Begin-----------------------------//
+// StringTokSample();
+// StringInString();
+// CharInString();
+//-------------------------cString Functions End-----------------------------//
+
+
+//-------------------------Dynamic Memory Allocation Begins------------------//
+// syntax    <target-type> * <ptr-name> = new <target-type>;
+//declare but do not initialize
+int * ptr1 = new int;
+//declare and initialize
+char * ch_ptr = new char('A');
+//initialize double with default value of 0;
+double * average_double = new double();
+//deallocate so there are no memory leaks
+delete ch_ptr;
+delete average_double;
+
+//dynamically allocate an array. you can't initialize an array in the new statement
+int number_elements = 10;
+char * str = new char[number_elements];
+delete [] str;
+/*
+allocates an array of 15 character pointers. Notice that the address returned is an address of an address of a character. 
+*/
+char ** star_star = new char * [15];
+
+
+
+//----------------------Dynamic Memory Allocation Ends------------------------//
+
+/*
+You would want to pass a pointer by reference if you have a need to modify the pointer rather than the object that the pointer is pointing to.
+*/
+
+//null pointer
+char * strA = nullptr;
+
+cout << "address of strA in  main: " << &strA << endl;
+//pass pointer as argument
+GetString(strA);
+cout << '\n' << strA << endl;
+delete [] strA;
+
+
+//-------------------Function Pointers---------------------------------------//
+char * shrooms[10] =
+{
+  "Matsutake", "Lobster", "Oyster", "King Boletus",
+  "Shaggy Mane", "Morel", "Chanterelle", "Calf Brain",
+  "Pigs Ear", "Chicken of the Woods"
+};
+
+int nums[10] = {99, 43, 23, 100, 66, 12, 0, 125, 76, 2};
+
+/*
+qsort
+    address of array
+    number of elements
+    size of each element
+    the address of the compare function to use
+*/
+qsort( (void *)shrooms, 10, sizeof(char *), compare_strs);
+qsort( (void *)nums, 10, sizeof( int *), compare_ints);
+
+cout << "Size of int *:  " << sizeof(int *) << endl;
+
+for(int i = 0; i < 10; i++)
+{
+  cout << shrooms[i] << " ," ;
+}
+
+cout << '\n\n';
+
+for(int i = 0; i < 10; i++)
+{
+  cout << nums[1] << " ,";
+}
+
+cout << '\n';
+
 return 0;
 }
 
+//-------------------------------------------------------------------------//
+//-------------------------END MAIN----------------------------------------//
+//-------------------------------------------------------------------------//
 
+
+
+
+
+//---------------Begin Function Definitions Outside of Main----------------//
 /*
 The function call passes the two addresses of integers; therefore, we need to catch these addresses in variables that can hold addresses: pointers
 */
@@ -196,6 +306,129 @@ int CharAt(char * str, char ch)
     return_value = static_cast<int> (cur - str);
   }
   return return_value;
+}
+
+void StringTokSample()
+{
+  char textTitle[] = "C++: An Active Learning Approach";
+  char * tokenPtr;
+
+  cout << "Our string to be tokenized is: " << textTitle
+  << "\n The Token/pieces are: \n";
+
+  tokenPtr = strtok(textTitle, " ");
+
+  while(tokenPtr != NULL)
+  {
+    cout << tokenPtr << '\n';
+    tokenPtr = strtok(NULL," ");
+  }
+  cout << "\n After strtok, textTitle = " << textTitle
+        <<endl;
+}
+
+void StringInString()
+{
+  char * ptr_ch;
+  char str[] = "Hello World";
+
+  cout << "Original string: " << str << endl;
+
+  ptr_ch = strstr(str, "World");
+  if(ptr_ch != NULL)
+  {
+    strncpy(ptr_ch, "Earth", 5);
+    cout << "\n Modified cString: " << str << endl;
+  }
+  else
+  {
+    cout << "\n The search string is not in the original string \n";
+  }
+}
+
+void CharInString()
+{
+  char * ptr_ch;
+  char str[] = "C++ An Active Approach";
+  cout << "Searching for all 'A' characters in " << str << endl;
+
+  //find the first occurrence of  n
+  ptr_ch = strchr(str, 'A');
+  if(ptr_ch == NULL)
+  {
+    cout << "The character is not in the string \n";
+  }
+  //find remaining occurrences of n
+  while(ptr_ch != NULL)
+  {
+    cout << "'A' found at " << ptr_ch - str + 1 << endl;
+    ptr_ch = strchr(ptr_ch + 1, 'A');
+  }
+}
+
+/*
+Below, we are catching the address of the pointer, not the contents of the pointer (what it points to, which is nullptr).
+
+You would want to pass a pointer by reference if you have a need to modify what the pointer points to rather than modifying the actual object that the pointer is pointing to when you start.
+
+this function starts off with the strA pointing to a stack address (0x00007ffeefbffa20) even though it was initialized as nullptr.  By the end of the function, we have switched to point to a heap address (0x0000000100205290) and added characters as contents in the array stored in heap.  The address where the strA variable is stored is not changed.
+
+char *& strA = the value of the first element in character array that strA points to
+
+
+*/
+void GetString(char *& strA)
+{
+  //declare a local array of characters with length of 256
+
+ char buffer[BUFFER_LENGTH];
+
+ cout << "Please enter a string: ";
+/*
+we flush the input buffer before and after our call to .getline. The first .ignore cleans the buffer prior to the .getline call. The second flush is used to clean any remaining characters in the buffer after the .getline function has executed.
+
+the .ignore member function removes a certain number of characters from the keyboard/input buffer. Everything in the parentheses for the .ignore function call allows us to find the number of characters currently in the keyboard buffer:
+
+  - rdbuf  returns a pointer to the stream buffer object currently associated with the stream (in this case "cin" or input buffer).
+
+  - in-avail returns the number of characters available to read in the input buffer identified by rdbuf
+*/
+
+ cin.ignore(cin.rdbuf()->in_avail());
+ cin.getline(buffer, BUFFER_LENGTH);
+
+ /*
+Although not intuitive, .getline sets a flag if it reads the maximum number of characters. If this happens, the .clear function call is required to reset any flags that may have been set by the .getline function.
+ */
+ cin.clear();
+ cin.ignore(cin.rdbuf()->in_avail());
+ cout << "Here is string entered: " << buffer << endl;
+
+/*
+new returns a pointer, thereby changing what strA pointer points to.
+The expression uses dynamic memory allocation to create a character array of a specified size in the heap memory.
+*/
+strA = new char[strlen(buffer) + 1];
+
+//now we copy the contents from buffer to the address that strA points to.
+strcpy(strA, buffer);
+
+}
+
+int compare_ints(const void * arg1, const void * arg2)
+{
+  int return_value = 0;
+  if( *(int *)arg1 < *(int *)arg2 )
+    return_value = -1;
+  else if(*(int *)arg1 > *(int *)arg2 )
+    return_value =1;
+  return return_value;
+}
+
+int compare_strs(const void * arg1, const void * arg2)
+{
+
+  return (strcmp ( *(char **) arg1, *(char **) arg2));
 }
 
 
